6ce5ffc69b0508837e588f7d4d6a57ac3b6544fd
fahmi200706 <fahmi.asyahdan@gmail.com>
2025-03-06 13:41:20 +0700

    Add files via upload
    Commit

---
+++ Class_B.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package soal2;
+
+/**
+ *
+ * @author DELL
+ */
+public class Class_B extends Class_A{
+    void callthis(){
+        System.out.println("Inside Class B's Method!");
+    }
+}
